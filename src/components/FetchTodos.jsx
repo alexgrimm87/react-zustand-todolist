59e179b1002b1c32b692ba3c46bee0ab4fe22a5f
Oleksandr <alexgrimm87@gmail.com>
2024-03-31 21:13:22 +0300

    replace shallow with newer and recommended useShallow

--- src/components/FetchTodos.jsx
+++ src/components/FetchTodos.jsx
@@ -1,16 +1,15 @@
-import {shallow} from 'zustand/shallow';
+import {useShallow} from 'zustand/react/shallow'; //fix rerender when checked/unchecked input
 import {Button} from '@chakra-ui/react';
 import {useTodos} from '../store';
 
 const FetchTodos = () => {
-  const {loading, error, fetchTodos} = useTodos(
-    (state) => ({
+  const {loading, error, fetchTodos} = useTodos(useShallow(
+  (state) => ({
       loading: state.loading,
       error: state.error,
       fetchTodos: state.fetchTodos
-    }),
-    shallow //fix rerender when checked/unchecked input
-  );
+    })
+  ));
 
   return (
     <Button isLoading={loading} onClick={fetchTodos}>
